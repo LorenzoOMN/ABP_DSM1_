0677233537ef870f92d524654c5568f1836efb09
Vitor Hirch <vitor.lshirch@gmail.com>
2026-06-07 13:15:15 -0300

    fix rota capitulo 5

--- src/app.js
+++ src/app.js
@@ -86,6 +86,11 @@ app.get("/capitulo4", function (_req, res) {
     res.render("capitulo4");
 });
 
+// Rota capítulo 5
+app.get("/capitulo5", function (_req, res) {
+  res.render("capitulo5");
+});
+
 
 // Rota do mapa
 app.get("/mapa", function (_req, res) {
